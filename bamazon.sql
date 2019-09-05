d8ad0b1cc100d3325c4991a83b6d8a4d1a8d6615
Fiona Zhou <fxtzhou@yahoo.com>
2019-09-04 17:39:34 -0700

    database working and pulling products seeds

--- bamazon.sql
+++ bamazon.sql
@@ -8,36 +8,9 @@ item_id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(100) NULL,
 department_name VARCHAR(100) NULL,
 price DECIMAL(10,4) NULL,
-stock_quantity INT NULL
+stock_quantity INT NULL,
+PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1, "Nintendo Switch", "Electronics", 299.99, 30);
+SELECT * from products 
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (2, "Collection of Mana - Nintendo Switch", "Video Games", 33.84, 1);
-
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (3, "Star Wars: Galaxy's Edge A Crash of Fate", "Books", 12.39, 50);
-
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (4, "Amazon Essentials Men's Athletic-Fit Stretch Jean", "Men's Clothing", 34.10, 10);
-
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (5, "Xbox One X 1TB Console - Gears 5 Limited Edition Bundle", "Electronics", 499.00, 4);
-
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (6, "
-VHT BCL0125 Clear Exact-Match Automotive Top Coat", "Automotive", 9.81, 18);
-
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (7, "Tide Pods", "Household", 9.99, 100);
-
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (8, "Charmin Ultra Soft Toilet Paper", "Household", 14.99, 50);
-
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (9, "Creatine", "Health", 39.99, 100);
-
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (10, "Advil Liquid Gels 800mg", "Health", 7.99, 100);
---
+++ products_seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES 
+(1, "Nintendo Switch", "Electronics", 299.99, 30),
+(2, "Collection of Mana - Nintendo Switch", "Video Games", 33.84, 1),
+
+(3, "Star Wars: Galaxy's Edge A Crash of Fate", "Books", 12.39, 50),
+(4, "Amazon Essentials Men's Athletic-Fit Stretch Jean", "Men's Clothing", 34.10, 10),
+
+(5, "Xbox One X 1TB Console - Gears 5 Limited Edition Bundle", "Electronics", 499.00, 4),
+
+(6, "VHT BCL0125 Clear Exact-Match Automotive Top Coat", "Automotive", 9.81, 18),
+
+(7, "Tide Pods", "Household", 9.99, 100),
+
+(8, "Charmin Ultra Soft Toilet Paper", "Household", 14.99, 50),
+
+(9, "Creatine", "Health", 39.99, 100),
+
+(10, "Advil Liquid Gels 800mg", "Health", 7.99, 100)
